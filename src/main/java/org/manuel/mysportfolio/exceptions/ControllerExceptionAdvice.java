59462bf3971f62be2808e8904ee81b14b340e8e9
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2019-11-01 13:46:38 +0100

    fixing compilation error

--- src/main/java/org/manuel/mysportfolio/exceptions/ControllerExceptionAdvice.java
+++ src/main/java/org/manuel/mysportfolio/exceptions/ControllerExceptionAdvice.java
@@ -1,21 +1,12 @@
 package org.manuel.mysportfolio.exceptions;
 
 import org.manuel.mysportfolio.exceptions.model.AppError;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
-import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionAdvice  {
