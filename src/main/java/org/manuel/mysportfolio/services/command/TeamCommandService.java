2e0755f949eeb3299e108cfe021cd7258cba68a2
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2019-11-14 15:41:14 +0100

    - post authorize error still committing. Changing it to pre authorize

--- src/main/java/org/manuel/mysportfolio/services/command/TeamCommandService.java
+++ src/main/java/org/manuel/mysportfolio/services/command/TeamCommandService.java
@@ -1,7 +1,6 @@
 package org.manuel.mysportfolio.services.command;
 
 import org.manuel.mysportfolio.model.entities.team.Team;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface TeamCommandService {
@@ -9,7 +8,7 @@ public interface TeamCommandService {
     @PreAuthorize("hasRole('ROLE_USER') and @permissionsService.canSaveTeam()")
     Team save(Team team);
 
-    @PostAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER') and returnObject.createdBy.get() == authentication.principal.attributes['sub']")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER') and @permissionsService.isTeamAdmin(#team.id)")
     Team update(Team team);
 
 }
