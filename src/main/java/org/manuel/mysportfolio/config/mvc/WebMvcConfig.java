f054346bf238b8c693476c41fd07348eba4ab5fa
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2020-10-24 18:26:12 +0200

    - Adding cors in spring web

--- src/main/java/org/manuel/mysportfolio/config/mvc/WebMvcConfig.java
+++ src/main/java/org/manuel/mysportfolio/config/mvc/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.manuel.mysportfolio.config.mvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -8,6 +9,11 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @Configuration
 @lombok.AllArgsConstructor
 class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
 
   @Override
   public void addInterceptors(final InterceptorRegistry registry) {
