b6f2151b16db13c25cfeb3be65e69bac0f15d617
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2020-10-26 13:15:30 +0100

    - Adding custom message when validation exception

--- src/main/java/org/manuel/mysportfolio/exceptions/ControllerExceptionAdvice.java
+++ src/main/java/org/manuel/mysportfolio/exceptions/ControllerExceptionAdvice.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.manuel.mysportfolio.exceptions.model.AppError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,11 +15,17 @@ public class ControllerExceptionAdvice {
   protected ResponseEntity<AppError> handleNotFound(final EntityNotFoundException ex,
       final HttpServletRequest request) {
     final String bodyOfResponse = ex.getMessage();
-
     final AppError appError = new AppError("v1", "404", bodyOfResponse, ex.getDomain(),
         "Entity not found", bodyOfResponse, "errorReportUri");
 
     return new ResponseEntity<>(appError, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+      final HttpServletRequest request) {
+    final AppError appError = AppError.fromMethodArgumentNotValidException(ex);
+    return new ResponseEntity<>(appError, HttpStatus.BAD_REQUEST);
   }
 
 }
--- src/main/java/org/manuel/mysportfolio/exceptions/model/AppError.java
+++ src/main/java/org/manuel/mysportfolio/exceptions/model/AppError.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @lombok.Data
 public class AppError {
@@ -12,11 +14,26 @@ public class AppError {
   private final String apiVersion;
   private final ErrorBlock error;
 
+  public AppError(final String apiVersion, final ErrorBlock error) {
+    this.apiVersion = apiVersion;
+    this.error = error;
+  }
+
   public AppError(final String apiVersion, final String code, final String message,
       final String domain,
       final String reason, final String errorMessage, final String errorReportUri) {
     this.apiVersion = apiVersion;
     this.error = new ErrorBlock(code, message, domain, reason, errorMessage, errorReportUri);
+  }
+
+  public static AppError fromMethodArgumentNotValidException(final MethodArgumentNotValidException ex) {
+    final var errors = ex.getBindingResult().getFieldErrors().stream().map(fieldError -> {
+      final var domain = ex.getBindingResult().getObjectName();
+      final var reason = fieldError.getField() + " " + fieldError.getDefaultMessage();
+      return new Error(domain, reason, reason, null);
+    }).collect(Collectors.toList());
+    final var errorBlock = new ErrorBlock("400", "Entity validation exception", errors);
+    return new AppError("v1", errorBlock);
   }
 
   public static AppError fromDefaultAttributeMap(final String apiVersion,
@@ -60,6 +77,13 @@ public class AppError {
       this.errors = Collections.singletonList(
           new Error(domain, reason, errorMessage, errorReportUri + "?id=" + uniqueId)
       );
+    }
+
+    public ErrorBlock(final String code, final String message, final List<Error> errors) {
+      this.code = code;
+      this.message = message;
+      this.uniqueId = UUID.randomUUID();
+      this.errors = errors;
     }
 
     public static ErrorBlock copyWithMessage(final ErrorBlock s, final String message) {
